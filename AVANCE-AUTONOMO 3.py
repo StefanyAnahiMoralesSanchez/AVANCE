671b4ca81438bd13967329fb1ae13af7db2017d0
StefanyAnahiMoralesSanchez <anahisp.morales@gmail.com>
2024-12-08 17:14:59 -0500

    Add files via upload

---
+++ AVANCE-AUTONOMO 3.py
@@ -0,0 +1,32 @@
+#Declaro palabras de las cuidades de Ecuador
+a=(str)
+# pantalla 1
+print('Bienvenido al juego del AHORCADO')
+    # Colocar imagen referente al juego
+# pantalla 2 
+print('REGISTRESE')
+print('Nombres completos:')
+b=(str)
+if(a==range(10)):
+    print('ERROR:Ingrese solo letras')
+print('Nombre de jugador:')
+c=(str)
+x=int(input('Ingresa tu edad:  '))
+print({x})
+if(x<15):
+    print('No puedes continuar')
+    exit()
+# pantalla 3
+y=int(input('Selecciona un nivel de juego:  '))
+z=1
+w=2
+v=3
+if(y==z):
+    print('Selecionaste el nivel facil')
+    if(y==w):
+        print('Seleccionaste el nivel medio')
+        if(y==v):
+            print('Selecionaste el nivel dificil')
+else:
+    (4<y>0)
+    print('Nivel seleccionado no existe, intente otra vez')
